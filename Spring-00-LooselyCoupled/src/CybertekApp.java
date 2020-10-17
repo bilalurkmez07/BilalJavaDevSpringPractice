dce4bb8ab45ba8da7bdebb9448aef5e9d183a8d4
bilalurkmez07 <urkmezbilal07@gmail.com>
2020-10-17 12:00:23 -0400

    Working on Tightly Coupled and Loosely Coupled

---
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -0,0 +1,21 @@
+import implementation.Mentor;
+import service.FullTimeMentor;
+import service.MentorAccount;
+import service.PartTimeMentor;
+
+public class CybertekApp {
+
+
+    public static void main(String[] args) {
+        //FullTimeMentor fullTime = new FullTimeMentor();
+
+        //MentorAccount mentor = new MentorAccount(fullTime);
+
+        PartTimeMentor partTime = new PartTimeMentor();
+
+        MentorAccount mentor = new MentorAccount(partTime);
+        mentor.manageAccount();
+
+
+    }
+}
